9ac84ab579da42ce8cec14fd594c3bcc8a041731
Shawn Harden <sharden007@gmail.com>
2023-02-23 21:49:06 -0500

    added sorting, searching etc.

--- src/components/MyTable.js
+++ src/components/MyTable.js
@@ -1,22 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TableSortLabel } from '@material-ui/core';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TableSortLabel,
+  TextField,
+  Box,
+} from '@material-ui/core';
 
 function MyTable() {
   const [data, setData] = useState([]);
   const [sortDirection, setSortDirection] = useState('asc');
   const [activeColumn, setActiveColumn] = useState('');
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-    axios.get('/data.json')
-      .then(response => setData(response.data))
-      .catch(error => console.log(error));
-  }, []);
+    axios
+      .get('/data.json')
+      .then((response) => {
+        const filteredData = response.data.filter((row) => {
+          const rowValues = Object.values(row).join('').toLowerCase();
+          return rowValues.includes(searchText.toLowerCase());
+        });
+        setData(filteredData);
+      })
+      .catch((error) => console.log(error));
+  }, [searchText]);
 
   const handleSortClick = (column) => {
     const isAsc = activeColumn === column && sortDirection === 'asc';
     setSortDirection(isAsc ? 'desc' : 'asc');
     setActiveColumn(column);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
   };
 
   const sortedData = data.sort((a, b) => {
@@ -34,50 +57,70 @@ function MyTable() {
   });
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <TableSortLabel
-                active={activeColumn === 'field1'}
-                direction={sortDirection}
-                onClick={() => handleSortClick('field1')}
-              >
-                Field 1
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel
-                active={activeColumn === 'field2'}
-                direction={sortDirection}
-                onClick={() => handleSortClick('field2')}
-              >
-                Field 2
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel
-                active={activeColumn === 'field3'}
-                direction={sortDirection}
-                onClick={() => handleSortClick('field3')}
-              >
-                Field 3
-              </TableSortLabel>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sortedData.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell>{row.field1}</TableCell>
-              <TableCell>{row.field2}</TableCell>
-              <TableCell>{row.field3}</TableCell>
+    <>
+      <Box display="flex" justifyContent="flex-end" mb={2}>
+        <TextField
+          label="Search"
+          variant="outlined"
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </Box>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <TableSortLabel
+                  active={activeColumn === 'AA'}
+                  direction={sortDirection}
+                  onClick={() => handleSortClick('AA')}
+                >
+                  AA
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={activeColumn === 'CAT'}
+                  direction={sortDirection}
+                  onClick={() => handleSortClick('CAT')}
+                >
+                  CAT
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={activeColumn === 'TN'}
+                  direction={sortDirection}
+                  onClick={() => handleSortClick('TN')}
+                >
+                  TN
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={activeColumn === 'ID'}
+                  direction={sortDirection}
+                  onClick={() => handleSortClick('ID')}
+                >
+                  ID
+                </TableSortLabel>
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {sortedData.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell>{row.AA}</TableCell>
+                <TableCell>{row.CAT}</TableCell>
+                <TableCell>{row.TN}</TableCell>
+                <TableCell>{row.ID}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
